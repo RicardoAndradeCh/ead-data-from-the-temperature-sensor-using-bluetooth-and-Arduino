ecb9edce2851f2552465cc4faa7b52dd480df3ec
RicardoAndradeCh <49264486+RicardoAndradeCh@users.noreply.github.com>
2019-04-18 08:44:00 -0500

    Add files via upload

---
+++ Lab8Ex1.ino
@@ -0,0 +1,17 @@
+float tempC;
+int pinLM35 = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+analogReference(INTERNAL);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+tempC = analogRead(pinLM35);
+tempC = (1.1 + tempC + 100.0)/1024.0;
+Serial.print(tempC);
+Serial.print("\n");
+delay(1000);
+}
---
+++ Lab8Ex2.ino
@@ -0,0 +1,33 @@
+  int tempC;
+  long tm,t,d;
+  int pinLM35 = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pinLM35, INPUT);
+  analogReference(INTERNAL);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tempC = analogRead(pinLM35);
+ // tempC = (1.1 + tempC + 100.0)/1024.0;
+
+  tm = millis();
+  t = tm /1000;
+  d = tm%1000;
+  Serial.flush();
+
+  Serial.print("time : ");
+  Serial.print(t);
+  Serial.print(".");
+  Serial.print(d);
+  Serial.print("s\t");
+
+  Serial.print("temperature: ");
+  Serial.print(tempC);
+  Serial.print("C");
+  Serial.print("\n");
+  delay(2000);
+}
